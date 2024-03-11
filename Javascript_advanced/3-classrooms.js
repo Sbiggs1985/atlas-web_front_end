86b265db98b5d0bd024f7f5ba10c2936c7bba998
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-03-11 09:58:14 -0500

    Submitting task 3

--- Javascript_advanced/3-classrooms.js
+++ Javascript_advanced/3-classrooms.js
@@ -1,1 +1,24 @@
+/** @format */
 
+function createClassRoom(numbersOfStudents) {
+  function studentSeat(seat) {
+    return function () {
+      return seat;
+    };
+  }
+
+  var students = [];
+
+  for (let i = 0; i < numbersOfStudents; i++) {
+    let seatNumber = i + 1;
+    students.push(studentSeat(seatNumber));
+  }
+
+  return students;
+}
+
+var classRoom = createClassRoom(10);
+
+console.log(classRoom[0]());
+console.log(classRoom[3]());
+console.log(classRoom[9]());
