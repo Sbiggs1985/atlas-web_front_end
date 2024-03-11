5593cf8ce7eafdb4d7ca10dab3341085d120e5b8
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-03-11 09:48:01 -0500

    Submitting task 2

--- Javascript_advanced/2-function_me.js
+++ Javascript_advanced/2-function_me.js
@@ -1,1 +1,15 @@
+/** @format */
 
+function welcomeMessage(fullName) {
+  return function () {
+    alert("Welcome " + fullName);
+  };
+}
+
+let guillaume = welcomeMessage("Guillaume");
+let alex = welcomeMessage("Alex");
+let fred = welcomeMessage("Fred");
+
+guillaume();
+alex();
+fred();
