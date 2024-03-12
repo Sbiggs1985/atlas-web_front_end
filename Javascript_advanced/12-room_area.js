c3eef727f9a20d9556d2076521c297e776437fd4
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-03-12 18:37:12 -0500

    update

--- Javascript_advanced/12-room_area.js
+++ Javascript_advanced/12-room_area.js
@@ -1,1 +1,19 @@
-// Create the 
+/** @format */
+
+// Create the 'roomDimensions' object
+// Creating an object named 'roomDimensions' that represents the dimensions of a room.
+// Two attributes: 'width' & 'length', which are set to 50 and 100 respectively.
+// Also has a method 'getArea', it calculates the surface area of the room using its width and length.
+
+const roomDimensions = {
+  width: 50,
+  length: 100,
+  getArea: function () {
+    return this.width * this.length;
+  },
+};
+
+const boundGetArea = roomDimensions.getArea.bind(roomDimensions);
+
+console.log(roomDimensions.getArea());
+console.log(boundGetArea());
